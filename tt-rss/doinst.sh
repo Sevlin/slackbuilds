982adf3c4fa554132e1c389163b4a0b6ddc71ded
Mykyta Solomko <sev@nix.org.ua>
2015-08-26 19:07:51 +0300

    Fixed path in doinst.sh

--- tt-rss/doinst.sh
+++ tt-rss/doinst.sh
@@ -1,5 +1,5 @@
 
-chmod 0775 var/www/webapps/tt-rss/cache
-chmod 0775 var/www/webapps/tt-rss/feed-icons
-chmod 0775 var/www/webapps/tt-rss/lock
+chmod 0775 var/www/tt-rss/cache
+chmod 0775 var/www/tt-rss/feed-icons
+chmod 0775 var/www/tt-rss/lock
 
