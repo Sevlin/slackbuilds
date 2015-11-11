7f1214651853f712af7674dc2b7cde5dfb197584
Mykyta Solomko <sev@nix.org.ua>
2015-11-11 21:47:50 +0200

    First run initialization

--- .ci/bin/run.sh
+++ .ci/bin/run.sh
@@ -16,12 +16,23 @@ readonly CI_ROOT='.ci'
 readonly BIN_DIR="${CI_ROOT}/bin"
 readonly TMP_DIR="${CI_ROOT}/tmp"
 readonly LOCK_FILE="${TMP_DIR}/.lock"
+readonly OLD_COMMIT_FILE="${TMP_DIR}/commit.txt"
 
 trap "rm -f ${LOCK_FILE}" EXIT TERM
 
 # Create tmp dir if missing
 if [ ! -d "${TMP_DIR}" ]; then
     mkdir -pv "${TMP_DIR}"
+fi
+
+# Set commit id to previous if missing
+if [ ! -f "${OLD_COMMIT_FILE}" ] \
+|| [ -z "$(cat ${OLD_COMMIT_FILE})" ]; then
+    prev_commit=$(git rev-parse HEAD~1)
+	pmsg 'i' "-> Updating last commit to previous commit ${COLBLU}${prev_commit}${COLNON}."
+	echo ${prev_commit} \
+	   > ${OLD_COMMIT_FILE}
+	unset prev_commit
 fi
 
 run_script()
