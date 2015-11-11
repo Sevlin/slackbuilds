656e24e1e2e497515fbfee820ee602a72c0bb9a0
Mykyta Solomko <sev@nix.org.ua>
2015-11-11 11:55:25 +0200

    tabs to spaces; skip noarch condition for phobos

--- .ci/bin/build.sh
+++ .ci/bin/build.sh
@@ -3,83 +3,90 @@
 set -e
 
 for LIBFILE in $(ls .ci/lib/*.sh); do
-	if [ -x "${LIBFILE}" ]; then
-		source "${LIBFILE}"
-	fi
+    if [ -x "${LIBFILE}" ]; then
+        source "${LIBFILE}"
+    fi
 done
 
 TMPDIR=${TMPDIR:-'.ci/tmp'}
+
+# phobos-NNN - i486 build server
+# deimos-NNN - amd64, noarch build server
+SKIP_NOARCH=${SKIP_NOARCH:-'yes'}
+if [[ "$(hostname)" =~ ^phobos-* ]]; then
+    SKIP_NOARCH='yes'
+fi
 
 readonly OLD_COMMIT_FILE="${TMPDIR}/commit.txt"
 readonly CHNG_SLKBLDS_FILE="${TMPDIR}/slackbuilds.txt"
 
 read_slckbld_changes()
 {
-	SLKBLDS_LIST=$(cat ${CHNG_SLKBLDS_FILE})
-	if [ -z "${SLKBLDS_LIST}" ]; then
-		pmsg 'e' 'Empty SlackBuilds list!'
-		exit 1
-	fi
+    SLKBLDS_LIST=$(cat ${CHNG_SLKBLDS_FILE})
+    if [ -z "${SLKBLDS_LIST}" ]; then
+        pmsg 'e' 'Empty SlackBuilds list!'
+        exit 1
+    fi
 }
 
 download_src()
 {
-	local sb_path=${1}
-	local sb_dir=$(dirname ${sb_path})
-	local sb_info="$(basename ${sb_path} .SlackBuild).info"
+    local sb_path=${1}
+    local sb_dir=$(dirname ${sb_path})
+    local sb_info="$(basename ${sb_path} .SlackBuild).info"
 
-	pushd ${sb_dir}
-		# Check availability of information file
-		if [ ! -r "${sb_info}" ]; then
-			pmsg 'e' "Missing ${sb_info}!"
-			pmsg 'e' "Can't download sources!"
-			exit 1
-		fi
-	popd
+    pushd ${sb_dir}
+        # Check availability of information file
+        if [ ! -r "${sb_info}" ]; then
+            pmsg 'e' "Missing ${sb_info}!"
+            pmsg 'e' "Can't download sources!"
+            exit 1
+        fi
+    popd
 
-	unset sb_path sb_dir sb_info
+    unset sb_path sb_dir sb_info
 }
 
 build_pkg()
 {
-	local sb_path=${1}
+    local sb_path=${1}
 
-	# Check file availability
-	if [ ! -f "${sb_path}" ]; then
-		pmsg 'e' "Missing SlackBuild ${sb_path}!"
-		exit 1
-	fi
+    # Check file availability
+    if [ ! -f "${sb_path}" ]; then
+        pmsg 'e' "Missing SlackBuild ${sb_path}!"
+        exit 1
+    fi
 
-	local sb_dir=$(dirname ${sb_path})
-	local sb_file=$(basename ${sb_path})
+    local sb_dir=$(dirname ${sb_path})
+    local sb_file=$(basename ${sb_path})
 
-	# cd into SlackBuild's dir and run
-	pushd ${sb_dir}
-		pmsg 'i' "Running build script ${COLYLW}${sb_file}${COLNON}..."
-		echo
-		exec fakeroot bash ${sb_file}
-		echo -e '\n\n\n'
-	popd
+    # cd into SlackBuild's dir and run
+    pushd ${sb_dir}
+        pmsg 'i' "Running build script ${COLYLW}${sb_file}${COLNON}..."
+        echo
+        exec fakeroot bash ${sb_file}
+        echo -e '\n\n\n'
+    popd
 
-	unset sb_path sb_dir sb_file
+    unset sb_path sb_dir sb_file
 }
 
 upd_last_commit()
 {
-	local cur_commit=$(git rev-parse HEAD)
-	if [ ! -z "${cur_commit}" ]; then
-		pmsg 'i' "Updating last commit to ${COLBLU}${cur_commit}${COLNON}."
-		echo ${cur_commit} \
-		   > ${OLD_COMMIT_FILE}
-	fi
-	unset cur_commit
+    local cur_commit=$(git rev-parse HEAD)
+    if [ ! -z "${cur_commit}" ]; then
+        pmsg 'i' "Updating last commit to ${COLBLU}${cur_commit}${COLNON}."
+        echo ${cur_commit} \
+           > ${OLD_COMMIT_FILE}
+    fi
+    unset cur_commit
 }
 
 read_slckbld_changes
 
 for SLKBLD in ${SLKBLDS_LIST[*]}; do
-	download_src "${SLKBLD}"
-	build_pkg "${SLKBLD}"
+    download_src "${SLKBLD}"
+    build_pkg "${SLKBLD}"
 done
 
 exit 0
