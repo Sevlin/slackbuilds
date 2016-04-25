979c9a37455c53d6e1899176dff7d938bc70bbae
Mykyta Solomko <sev@nix.org.ua>
2016-04-25 11:51:37 +0300

    ADD: zabbix {server,proxy,agent,web}

---
+++ zabbix_server/doinst.sh
@@ -0,0 +1,44 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+add_user_group() {
+  ZABBIXGROUP='%ZABBIXGROUP%'
+  ZABBIXGID=%ZABBIXGID%
+  ZABBIXUSER='%ZABBIXUSER%'
+  ZABBIXUID=%ZABBIXUID%
+  # Add group if missing
+  if ! grep -q "^${ZABBIXGROUP}:" /etc/group ; then
+    groupadd -g ${ZABBIXGID} ${ZABBIXGROUP}
+  fi
+  # Add user if missing
+  if ! grep -q "^${ZABBIXUSER}:" /etc/passwd ; then
+    useradd -u ${ZABBIXUID} -g ${ZABBIXGROUP} -d /dev/null -s /bin/false -c "User for Zabbix server" ${ZABBIXUSER}
+  fi
+}
+
+add_user_group
+preserve_perms etc/rc.d/rc.zabbix_server.new
+config etc/zabbix/zabbix_server.conf.new
+config var/log/zabbix/zabbix_server.log.new
+rm -f var/log/zabbix/zabbix_server.log.new
